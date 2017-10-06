9f580290b0a8d5e053e1ab5485c33b89b0ce7ef3
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2017-10-06 16:26:29 +0200

    Add basic functionality with filename and delimiter

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import csv
-filename = 'box-prediction/products_per_supplier.csv'
+from sys import argv
+
+filename = argv[1]
+
+if len(argv) == 3:
+    delimiter = argv[2]
+else:
+    delimiter = ','
+
 with open(filename, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=delimiter)
     header = next(csvreader)
     lengths = [0]*len(list(header))
+    content = []
     for row in csvreader:
+        row_content = []
         for i, cell in enumerate(row):
             lengths[i] = max(len(cell), lengths[i])
-    lengths = [l + 1 for l in lengths]
-    print(lengths)
-    s = ""
-    for i, l in enumerate(lengths):
-        s += "{}"
-        row = "%s"*len(lengths)
+            row_content.append(cell)
+        content.append(row_content)
+
+lengths = [l + 1 for l in lengths]
+row_length = len(lengths)
+output_string = ''
+
+for row in content:
+    for i in range(row_length):
+        output_string += '{0:>{width}}'.format(row[i], width=lengths[i])
+    output_string += '\n'
+
+# Remove last newline
+output_string = output_string.strip()
+
+print(output_string.strip())
