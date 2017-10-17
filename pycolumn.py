bce6963d91f79843f413edf4955221b64e4918bf
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2017-10-17 18:43:29 +0200

    Compartmentize body into smaller functions

--- pycolumn.py
+++ pycolumn.py
@@ -5,13 +5,7 @@ from sys import exit
 import argparse
 from argparse import RawTextHelpFormatter
 
-def print_usage_and_exit(parser, string=None):
-    if string: print(string)
-    parser.print_usage()
-    exit()
-
-def main():
-    # Set up argparse
+def setup_argparse():
     parser = argparse.ArgumentParser(
         description='Command line utility for pretty printing csv files.',
         formatter_class=RawTextHelpFormatter,
@@ -22,33 +16,43 @@ def main():
         help='separator/delimiter used in the csv file\ndefault is ,')
     parser.add_argument('-w', '--width', type=int, default=1,
         help='width of space between columns\ndefault is 1')
-    parser.add_argument('-r', '--rows', type=int, default=-1,
+    parser.add_argument('-r', '--rows', type=int, default=1000,
         help='number of rows to show\ndefault is 1000')
     args = parser.parse_args()
+    return args
 
-    filename = args.filename
-
+def read_content(filename, max_rows, separator, spacing):
     with open(filename, 'r') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=args.separator)
+        csvreader = csv.reader(csvfile, delimiter=separator)
         header = next(csvreader)
         lengths = [len(i) for i in list(header)]
         content = [header]
         for num_rows, row in enumerate(csvreader):
             row_content = []
-            if num_rows < args.rows - 1:
+            if num_rows < max_rows - 1:
                 for i, cell in enumerate(row):
                     lengths[i] = max(len(cell), lengths[i])
                     row_content.append(cell)
                 content.append(row_content)
+    lengths = [l + spacing for l in lengths]
+    return content, lengths
 
-    lengths = [l + args.width for l in lengths]
-    num_rows = len(lengths)
-
+def print_output(content, lengths):
     for row in content:
         output = ''
-        for i in range(num_rows):
+        for i in range(len(lengths)):
             output += '{0:>{width}}'.format(row[i], width=lengths[i])
         print(output)
+
+def main():
+    args = setup_argparse()
+    content, lengths = read_content(
+        filename=args.filename,
+        max_rows=args.rows,
+        separator=args.separator,
+        spacing=args.width
+    )
+    print_output(content, lengths)
 
 if __name__ == '__main__':
     main()
