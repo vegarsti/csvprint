85c0b0a81e4753b85c33b9ffe00aebe212207ff5
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-03-02 13:44:55 +0100

    Delete stupid files

--- CsvObject.py
+++
@@ -1,8 +0,0 @@
-class CsvObject(object):
-    def __init__(self, number_of_rows, number_of_columns):
-        self.number_of_rows = number_of_rows
-        self.number_of_columns = number_of_columns
-        self.data = 
-
-    def __getitem__(self, row_number):
-        return self.data[row_number]
