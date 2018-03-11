119269053237e3a9f2e5aba99144ec0c00698743
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-03-11 18:39:39 +0100

    Add parametrized test cases

---
+++ test.py
@@ -0,0 +1,22 @@
+from csvprint import *
+import sys
+from test_cases import *
+import pytest
+
+@pytest.mark.parametrize("args,expected_output", [
+    ('imdb.csv', expected_normal_output),
+    ('imdb.csv --markdown', expected_markdown_output),
+    ('imdb.csv --j l', expected_normal_output),
+    ('imdb.csv --j r', expected_right_justified_output),
+    ('example.tsv -s \\t', expected_tab_output),
+    ('imdb.csv --header', expected_header_output)
+])
+def test_correct_features(args, expected_output):
+    sys.argv = [sys.argv[0]]
+    parser = create_parser()
+    for arg in args.split():
+        sys.argv.append(arg)
+    args = parse_cli_arguments(parser)
+    store_content(parser, args)
+    output = get_output(args)
+    assert output == expected_output
