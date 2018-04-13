26a80a2c62b50dbff27768e20f0cbf540eabd62e
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-04-13 10:52:09 +0200

    Add documentation and improve naming

--- csvprint/printer.py
+++ csvprint/printer.py
@@ -1,33 +1,39 @@
 def copy_nested_list(l):
+    """Return a copy of list l to one level of nesting"""
     return [list(i) for i in l]
 
-def normalize_columns(content, num_columns):
-    new_content = copy_nested_list(content)
-    for row in new_content:
-        while len(row) < num_columns:
+def normalize_table(table, n):
+    """Return a normalized version of table such that it has n columns in each row, possibly with empty cells"""
+    normalized_table = copy_nested_list(table)
+    for row in normalized_table:
+        while len(row) < n:
             row.append('')
-    return new_content
+    return normalized_table
 
-def get_column_widths(content):
-    return [max(map(len, col)) for col in zip(*content)]
+def column_widths(table):
+    """Get the maximum size for each column in table"""
+    return [max(map(len, col)) for col in zip(*table)]
 
-def pad_cells(content, justify):
-    column_widths = get_column_widths(content)
-    new_content = copy_nested_list(content)
-    for row in new_content:
+def justify_table(table, justify):
+    """Return table justified according to justify"""
+    widths = column_widths(table)
+    new_table = copy_nested_list(table)
+    for row in new_table:
         num_cells = len(row)
         for cell_num, cell in enumerate(row):
             row[cell_num] = '{:{align}{width}}'.format(
                 cell,
                 align=justify[cell_num],
-                width=column_widths[cell_num],
+                width=widths[cell_num],
             )
-    return new_content
+    return new_table
 
 def header_line(length, border='-'):
+    """Return header of length"""
     return border*length
 
 def md_justification(justification):
+    """Given justification option, return corresponding markdown"""
     if justification == '<':
         return ':', '-'
     elif justification == '>':
@@ -35,35 +41,42 @@ def md_justification(justification):
     else:
         return '-', '-'
 
-def markdown_header(content, justify):
-    widths = get_column_widths(content)
-    content = []
+def markdown_header(table, justify):
+    """Get markdown header for table according to given justification options"""
+    widths = column_widths(table)
+    header_line = []
     for i, w in enumerate(widths):
         if w < 3:
             raise ValueError
         left, right = md_justification(justify[i])
-        content.append(left + '-'*(w-2) + right)
-    return content
+        header_line.append(left + '-'*(w-2) + right)
+    return header_line
 
-def add_header(content, markdown, header, justify):
-    widths = get_column_widths(content)
-    new_content = copy_nested_list(content)
-    if markdown:
-        new_content.insert(
-            1,
-            markdown_header(content, justify),
+def add_markdown_header(table, justify):
+    """Add markdown header lines to table"""
+    widths = column_widths(table)
+    new_table = copy_nested_list(table)
+    new_table.insert(
+        1,
+        markdown_header(table, justify),
+    )
+    return new_table
+
+def add_header(table, justify):
+    """Add header lines to table"""
+    widths = column_widths(table)
+    new_table = copy_nested_list(table)
+    for index in (0, 2):
+        new_table.insert(
+            index,
+            [header_line(w) for w in widths],
         )
-    if header:
-        for index in (0, 2):
-            new_content.insert(
-                index,
-                ['-'*w for w in widths],
-            )
-    return new_content
+    return new_table
 
-def add_padding(content, padding):
-    new_content = copy_nested_list(content)
-    for i, row in enumerate(new_content):
+def add_padding(table, padding):
+    """Return a version of table which is padded according to inputted padding"""
+    new_table = copy_nested_list(table)
+    for i, row in enumerate(new_table):
         padding_string = ' '*padding
         for j, cell in enumerate(row):
             left = padding_string
@@ -72,25 +85,32 @@ def add_padding(content, padding):
                 left = ''
             elif j == len(row) - 1:
                 right = ''
-            new_content[i][j] = left + new_content[i][j] + right
-    return new_content
+            new_table[i][j] = left + new_table[i][j] + right
+    return new_table
 
-def add_divider(content, decorator):
-    return [decorator.join(row).rstrip() for row in content]
+def join_columns_with_divider(table, decorator):
+    """Join each line in table with the decorator string between each cell"""
+    return [decorator.join(row).rstrip() for row in table]
 
-def join_lines(content):
-    return '\n'.join(content)
+def join_formatted_lines(lines):
+    """Return the finished output"""
+    return '\n'.join(lines)
 
 def run_pipeline(args):
-    content = normalize_columns(args['content'], args['num_columns'])
-    content = pad_cells(content, args['justify'])
-    content = add_padding(content, args['padding'])
-    content = add_header(
-        content,
-        args['markdown'],
-        args['header'],
-        args['justify']
-    )
-    content = add_divider(content, args['decorator'])
-    output = join_lines(content)
-    return output
+    raw_table = args['content']
+    number_of_columns = args['num_columns']
+    justification = args['justify']
+    padding = args['padding']
+    output_as_markdown = args['markdown']
+    output_as_header = args['header']
+    decorator = args['decorator']
+    normalized_table = normalize_table(raw_table, number_of_columns)
+    justified_table = justify_table(normalized_table, justification)
+    padded_table = add_padding(justified_table, padding)
+    if output_as_markdown:
+        padded_table = add_markdown_header(padded_table, justification)
+    if output_as_header:
+        padded_table = add_header(padded_table, justification)
+    lines = join_columns_with_divider(padded_table, decorator)
+    finished_output = join_formatted_lines(lines)
+    return finished_output
