f5594ffb46f6b1d6b16de49453404702916e9ca1
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2018-10-16 15:42:12 +0200

    Blacken setup file

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
-with open('README.md', 'r') as readme:
+with open("README.md", "r") as readme:
     LONG_DESCRIPTION = readme.read()
 
 setup(
-    name='csvprint',
-    version='0.5.0',
-    description='Print csv files in columnated format, either plain or as a Markdown or LaTeX table',
+    name="csvprint",
+    version="0.5.0",
+    description="Print csv files in columnated format, either plain or as a Markdown or LaTeX table",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    packages=['csvprint'],
-    license='MIT',
-    scripts=['bin/csvprint'],
-    author='Vegard Stikbakke',
-    author_email='vegard.stikbakke@gmail.com',
-    url='https://github.com/vegarsti/csvprint',
-    keywords='csv',
+    packages=["csvprint"],
+    license="MIT",
+    scripts=["bin/csvprint"],
+    author="Vegard Stikbakke",
+    author_email="vegard.stikbakke@gmail.com",
+    url="https://github.com/vegarsti/csvprint",
+    keywords="csv",
 )
